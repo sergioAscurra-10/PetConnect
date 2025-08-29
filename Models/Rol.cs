50d00ab382275e32597f0f1b4e91f0d40db379ba
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-08-28 21:00:07 -0500

    clase rol

---
+++ Models/Rol.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetConnect.Models
+{
+    [Table("Roles")]
+    public class Rol
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        // Propiedad de navegación para la relación muchos a muchos con Usuarios
+        public ICollection<UsuarioRol> UsuarioRoles { get; set; } = new List<UsuarioRol>();
+        }
+}
