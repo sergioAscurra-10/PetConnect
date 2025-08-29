11f6ebf00ccc96b621386bb72d945cfb22b87919
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-08-28 21:03:52 -0500

    configuracion dbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PetConnect.Models;
 
 namespace PetConnect.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Rol> Roles { get; set; }
+    public DbSet<UsuarioRol> UsuarioRoles { get; set; }
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuración para la clave primaria compuesta de la tabla UsuarioRoles
+        modelBuilder.Entity<UsuarioRol>()
+            .HasKey(ur => new { ur.UsuarioId, ur.RolId });
+
+        // Configuración de la relación muchos a muchos entre Usuario y Rol
+        modelBuilder.Entity<UsuarioRol>()
+            .HasOne(ur => ur.Usuario)
+            .WithMany(u => u.UsuarioRoles)
+            .HasForeignKey(ur => ur.UsuarioId);
+
+        modelBuilder.Entity<UsuarioRol>()
+            .HasOne(ur => ur.Rol)
+            .WithMany(r => r.UsuarioRoles)
+            .HasForeignKey(ur => ur.RolId);
+            
+        // Puedes añadir aquí más configuraciones "fluent API" si son necesarias
     }
 }
